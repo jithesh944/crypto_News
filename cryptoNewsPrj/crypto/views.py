a784b67ee3e54fd2400c035c530a3b14a95f0365
Jithesh Sunny <jithesh944@gmail.com>
2021-07-07 22:55:23 +0530

    Update the views with price api call

--- cryptoNewsPrj/crypto/views.py
+++ cryptoNewsPrj/crypto/views.py
@@ -6,10 +6,14 @@ import json,pprint
 
 # The home screen view function
 def home(request):
-    
-    api_request = requests.get('https://min-api.cryptocompare.com/data/v2/news/?lang=EN')
-    api_data = json.loads(api_request.content)
-    # print(api_data)
-    # api_data_pprint = pprint.pprint(api_data)
+    # Api call for getting Crypto Prices
+    api_price_request = requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP&tsyms=USD')
+    api_price_data = json.loads(api_price_request.content)
 
-    return render(request,'home.html',{"api": api_data})  
+
+    # API call fro getting crypto news
+    api_news_request = requests.get('https://min-api.cryptocompare.com/data/v2/news/?lang=EN')
+    api_news_data = json.loads(api_news_request.content)
+
+
+    return render(request,'home.html',{"api_news": api_news_data,"api_price":api_price_data})  
