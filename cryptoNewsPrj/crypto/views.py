08cd8a28eb0363ff16c56f11e1ed5ec6253187a1
Jithesh Sunny <jithesh944@gmail.com>
2021-07-11 11:32:34 +0530

    dropdown filed for currency

--- cryptoNewsPrj/crypto/views.py
+++ cryptoNewsPrj/crypto/views.py
@@ -1,19 +1,31 @@
 from django.shortcuts import render
 import requests
 import json
+from django.http import HttpResponse, response
+import os.path  
+
 
 # Create your views here.
 
 # The home screen view function
 def home(request):
-    # Api call for getting Crypto Prices
-    api_price_request = requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP,ETH,USDT,BNB,ADA,DOGE,BCH&tsyms=USD,JPY')
-    api_price_data = json.loads(api_price_request.content)
 
+    # Api call for getting Crypto Prices
+    if request.method == "POST":
+        currency = request.POST['currency']
+        price_url = "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP,ETH,USDT,BNB,ADA,DOGE,BCH&tsyms="+currency
+        api_price_request = requests.get(price_url)
+        # print(price_url)
+        api_price_data = json.loads(api_price_request.content)
+    else:
+        api_price_request = requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP,ETH,USDT,BNB,ADA,DOGE,BCH&tsyms=JPY')
+        api_price_data = json.loads(api_price_request.content)
 
     # API call fro getting crypto news
     api_news_request = requests.get('https://min-api.cryptocompare.com/data/v2/news/?lang=EN')
     api_news_data = json.loads(api_news_request.content)
 
 
-    return render(request,'home.html',{"api_news": api_news_data,"api_price":api_price_data})  
+    return render(request,'home.html',{"api_news": api_news_data,
+                                        "api_price":api_price_data,
+                                        'currency_selected':currency})  
